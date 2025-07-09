29491242201add4950b92ba1eb992c3bc64c84c3
1syunus <144527091+1syunus@users.noreply.github.com>
2025-07-09 19:20:55 +0100

    add search route

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { JIKAN_API_URL, transformJikanAnime } from "@/lib/jikan";
+import { encode } from "punycode";
+
+export async function GET(request: Request) {
+    try {
+        const {searchParams} = new URL(request.url)
+        const query = searchParams.get("q") || ""
+        const page = searchParams.get("page") || "1"
+        const limit = searchParams.get("limit") || "24"
+
+        if (!query) {
+            return NextResponse.json([])
+        }
+
+        const jikanUrl = `${JIKAN_API_URL}/anime?q=${encodeURIComponent(query)}$sfw&page=${page}&limit=${limit}`
+        console.log(`fetching browse data from ${jikanUrl}`)
+
+        const jikanResponse = await fetch(jikanUrl)
+        if (!jikanResponse.ok) {
+            const errorData = await jikanResponse.json()
+            throw new Error(`Jikan API error: ${jikanResponse.status} - ${errorData.message || jikanResponse.statusText}`)            
+        }
+        const data = await jikanResponse.json()
+        const transformedData = data.data ? data.data.map(transformJikanAnime) : []
+        return NextResponse.json(transformedData)    
+    } catch (error: any) {
+        console.error("Error fetching Jikan browse API:", error)
+        return NextResponse.json(
+            {message: "Failed to fetch data", error: error.message || "Unknown error"},
+            {status: 500}
+        )
+    }
+    
+}
