c51243fafd27554e40af6a9c968835660dc4d921
1syunus <144527091+1syunus@users.noreply.github.com>
2025-07-09 15:32:43 +0100

    add startup type defs - interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+// type defs
+export interface AnimeData {
+  id: number
+  title: string
+  description: string
+  type: "Series" | "Movie" | "OVA"
+  audioLanguages: string[]
+  subtitleLanguages: string[]
+  status: "Ongoing" | "Completed" | "New This Week"
+  year: number
+  episodes: number
+  rating: number
+  genres: string[]
+  image: string
+  heroImage?: string
+  trailerUrl?: string
+  duration: string
+}
+
+export interface ApiData {
+  availableAudioLanguages: string[]
+  availableSubtitleLanguages: string[]
+  availableGenres: string[]
+  contentTypes: string[]
+  statusOptions: string[]
+}
+
+export interface FilterOptions {
+  genre: string | null
+  type: string | null
+  status: string | null
+  // tbc
+}
