e79ca7cfe287292f5bd4cbbcd8b1e9bf6cf07824
1syunus <144527091+1syunus@users.noreply.github.com>
2025-08-01 09:29:15 +0100

    FEAT(FilterSection): update onChange to accept Genre values

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -601,7 +601,10 @@ const [hasMore, setHasMore] = useState<boolean>(true)
               <input
               type="checkbox"
               checked={isActive}
-              onChange={() => onFilterChange(category, optionValue)}
+              onChange={() => onFilterChange(category, {
+                id: (option as Genre).id,
+                name: (option as Genre).name
+              })}
               className="sr-only"
               />
               <div className={`w-4 h-4 rounded border-2 mr-3 flex items-center justify-center transition-all
