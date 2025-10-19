e21e4a913e9083f6f520264cf27524ae42492537
Suhayb Yunus <144527091+1syunus@users.noreply.github.com>
2025-10-19 10:25:24 +0100

    feat: add Vercel Analytics (#12)
    - Integrates Vercel's built-in analytics to provide insights into user traffic, demographics, and application performance.
    
    - This allows us to track user engagement and monitor the app's performance in production as a first step in its lifecycle.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import "../globals.css"
 import {Metadata} from "next"
 import Script from "next/script"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({
   children,
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         {children}
         <div id="modal-root"/>
+        <Analytics />
       </body>
     </html>
   );
